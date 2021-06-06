af3ce6ed922b426209101864d5ce1ed760c9d18e
Natalia <m108660013@mail.shu.edu.tw>
2021-06-06 20:14:28 +0800

    Part 5 & Part 6 update

--- app.py
+++ app.py
@@ -440,6 +440,6 @@ def getorders(orderNumber):
 	else:
 		return Response(json.dumps({"error":True,"message":"伺服器錯誤"}),mimetype="application/json"),500
 
-# app.run( host="0.0.0.0", port=3000,debug= True)
-app.run(port=3000,debug= True)
+app.run( host="0.0.0.0", port=3000,debug= True)
+# app.run(port=3000,debug= True)
 	
